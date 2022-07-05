bf334c75821c3627d94e3cf45e37b77f14cc62b8
takumi <konnitiha@outlook.com>
2022-07-05 15:34:53 +0900

    add three and build

---
+++ src/components/three/test.js
@@ -0,0 +1,36 @@
+import React, { useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+const Box = () => {
+  const ref = useRef(null);
+  const [isHovered, setIsHovered] = useState(false);
+
+  useFrame(() => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  }, []);
+
+  return (
+    <mesh
+      ref={ref}
+      onPointerOver={() => setIsHovered(true)}
+      onPointerOut={() => setIsHovered(false)}
+    >
+      <boxBufferGeometry args={isHovered ? [1.2, 1.2, 1.2] : [1, 1, 1]} />
+      <meshLambertMaterial color={isHovered ? 0x44c2b5 : 0x9178e6} />
+    </mesh>
+  );
+};
+
+const Main = () => {
+  return (
+    <Canvas dpr={2}>
+      <color attach="background" args={[0xf5f3fd]} />
+      <ambientLight intensity={0.5} />
+      <directionalLight intensity={0.5} position={[-10, 10, 10]} />
+      <Box />
+    </Canvas>
+  );
+}
+
+export default Main
