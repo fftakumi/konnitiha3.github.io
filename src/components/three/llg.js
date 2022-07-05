430cfedfb69ca88fd82a749a96a3b33ca3c7a1cd
takumi <konnitiha@outlook.com>
2022-07-05 19:14:56 +0900

    add plotly

--- src/components/three/llg.js
+++ src/components/three/llg.js
@@ -1,17 +1,22 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, {useRef} from "react";
+import { Canvas, useThree} from "@react-three/fiber";
+import { OrbitControls, TransformControls, ContactShadows, useGLTF, useCursor } from '@react-three/drei'
 
 const LLG = () => {
+  const mesh = useRef(null)
   return (
     <div id="canvas-container">
       <Canvas>
-        <mesh>
+        <TransformControls object={mesh} mode="translate">
+        <mesh ref={mesh}>
           <ambientLight intensity={0.1} />
           <directionalLight color="red" position={[0, 0, 5]} />
           <boxGeometry  args={[2, 2, 2]} />
           <meshStandardMaterial />
           <arrowHelper/>
         </mesh>
+        </TransformControls>
+        <OrbitControls/>
       </Canvas>
     </div>
   );
