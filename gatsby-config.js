a9d73f21f69bfd20266e7f6411d7dbe6287c1593
takumi <konnitiha@outlook.com>
2022-07-13 12:27:02 +0900

    add remark image

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,14 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
         remarkPlugins: [
           require('remark-math'),
         ],
